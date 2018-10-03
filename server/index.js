3cb1138872cfeaccc4fb710dfb2ed26df1c1fb79
Tim Dill <brodt7258@gmail.com>
2018-10-03 17:15:15 -0400

    add nginx

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const pgClient = new Pool({
 pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT')
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err));
 
 const redis = require('redis');
